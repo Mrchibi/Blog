dd3724e6e35eb844b2c3535a2503f8f6428e62b2
Jianyu <bb1034084869@163.com>
2017-02-08 17:39:33 +0800

    11 commit

--- app/main/views.py
+++ app/main/views.py
@@ -40,7 +40,7 @@ def index():
         post = Post(body=form.body.data,
                     author=current_user._get_current_object())
         db.session.add(post)
-		db.session.commit()
+		
 		
         return redirect(url_for('.index'))
     page = request.args.get('page', 1, type=int)
@@ -80,7 +80,7 @@ def edit_profile():
         current_user.location = form.location.data
         current_user.about_me = form.about_me.data
         db.session.add(current_user)
-		db.session.commit()
+		
         flash('Your profile has been updated.')
         return redirect(url_for('.user', username=current_user.username))
     form.name.data = current_user.name
